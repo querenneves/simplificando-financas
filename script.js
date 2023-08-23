0fcc43fbcfb2b1f65cc20b6325bfe9b4d0c3c101
querenneves <querensantos1@hotmail.com>
2023-08-22 22:48:08 -0300

    Ajustando botão para adicionar, falta ajustar exportação e opção de inclusão de logo no doc

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-// Define an array to store transactions
 let transactions = [];
+let balance = 0;
 
-// Function to add a new transaction
 function addTransaction() {
-    const description = document.getElementById("description").value;
-    const amount = parseFloat(document.getElementById("amount").value);
-    const date = document.getElementById("date").value;
-    const paymentMethod = document.getElementById("paymentMethod").value;
-    
-    // Create a new transaction object
-    const transaction = {
-        description: description,
-        amount: amount,
-        date: date,
-        paymentMethod: paymentMethod
-    };
-    
-    // Add the transaction to the array
-    transactions.push(transaction);
-    
-    // Update the transaction list and balance
+    const description = document.getElementById('description').value;
+    const amount = parseFloat(document.getElementById('amount').value);
+    const date = document.getElementById('date').value;
+    const paymentMethod = document.getElementById('paymentMethod').value;
+
+    if (description.trim() === '' || isNaN(amount) || date.trim() === '' || paymentMethod.trim() === '') {
+        alert('Por favor, preencha todos os campos corretamente.');
+        return;
+    }
+
+    transactions.push({ description, amount, date, paymentMethod });
     updateTransactionList();
     updateBalance();
 }
 
-// Function to update the transaction list
 function updateTransactionList() {
-    const transactionList = document.getElementById("transactionList");
-    transactionList.innerHTML = ""; // Clear the list
-    
-    transactions.forEach(transaction => {
-        const listItem = document.createElement("li");
-        listItem.textContent = `${transaction.description} - R$ ${transaction.amount.toFixed(2)} - ${transaction.date} - ${transaction.paymentMethod}`;
+    const transactionList = document.getElementById('transactionList');
+    transactionList.innerHTML = '';
+
+    for (const transaction of transactions) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${transaction.description}: R$ ${transaction.amount.toFixed(2)} - Data: ${transaction.date} - Forma de Pagamento: ${transaction.paymentMethod}`;
         transactionList.appendChild(listItem);
-    });
+    }
 }
 
-// Function to update the balance
 function updateBalance() {
-    const balanceAmount = document.getElementById("balanceAmount");
-    const totalAmount = transactions.reduce((total, transaction) => total + transaction.amount, 0);
-    balanceAmount.textContent = `R$ ${totalAmount.toFixed(2)}`;
+    balance = transactions.reduce((total, transaction) => total + transaction.amount, 0);
+    const balanceAmount = document.getElementById('balanceAmount');
+    balanceAmount.textContent = `R$ ${balance.toFixed(2)}`;
 }
 
-// Function to export transactions (not implemented here)
 function exportTransactions() {
-    // Implement your export logic here
-}
+    if (transactions.length === 0) {
+        alert('Não há registros para exportar.');
+        return;
+    }
 
-// Function to handle logo upload (not implemented here)
-function handleLogoUpload(event) {
-    // Implement your logo upload logic here
-}
+    const worksheet = XLSX.utils.json_to_sheet(transactions);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Registros Financeiros');
 
-// Attach click event listener to the "Adicionar Transação" button
-const addTransactionButton = document.getElementById("addTransactionButton");
-addTransactionButton.addEventListener("click", addTransaction);
+    const filename = 'registros_financeiros.xlsx';
+    XLSX.writeFile(workbook, filename);
+}
